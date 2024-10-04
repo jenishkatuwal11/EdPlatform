5934b20c6d2603c395cf02329226114ba6b71d43
PseudoCoder11 <jenishkatuwal7@gmail.com>
2024-10-04 13:10:27 +0545

    adding dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // Import the Vite React plugin
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
